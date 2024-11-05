d6ac8e5c7afcdb04dace238b2cecfb4d60cb88ae
Moshe Tanzer <tanzermt@gmail.com>
2024-11-06 00:20:01 +0200

    chore: add some frontend routes

---
+++ app/pages/(auth)/signin.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['protected-from-logged-in']
+})
+const email = ref('')
+const password = ref('')
+const status = ref('')
+const route = useRoute()
+async function signIn() {
+  try {
+    await $fetch('/api/auth/signin', {
+      method: 'POST',
+      body: {
+        email: email.value,
+        password: password.value
+      }
+    })
+    if (route.query.redirect) {
+      // this is safe since nuxt will throw error if redirect is out of app
+      navigateTo(route.query.redirect as string)
+    } else {
+      navigateTo('/')
+    }
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      status.value = error.message
+    } else {
+      status.value = 'Unknown error'
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Sign In</h1>
+    <form
+      method="post"
+      @submit.prevent="signIn"
+    >
+      <label for="email">Email</label>
+      <input
+        id="email"
+        v-model="email"
+        type="email"
+        autocomplete="username"
+        name="email"
+      >
+      <label for="password">Password</label>
+      <input
+        id="password"
+        v-model="password"
+        type="password"
+        autocomplete="current-password"
+        name="password"
+      >
+      <div>{{ status }}</div>
+
+      <button type="submit">
+        Sign In
+      </button>
+    </form>
+    <NuxtLink
+      to="/reset-password"
+      role="button"
+    >
+      forgot password
+    </NuxtLink>
+
+    <NuxtLink
+      to="/signup"
+      role="button"
+    >
+      signup
+    </NuxtLink>
+  </div>
+</template>
---
+++ app/pages/(auth)/signup.vue
@@ -0,0 +1,82 @@
+<script lang="ts" setup>
+const fname = ref('')
+const lname = ref('')
+const email = ref('')
+const password = ref('')
+const confirmPassword = ref('')
+const status = ref('')
+
+async function signUp() {
+  try {
+    await $fetch('/api/auth/signup', {
+      method: 'POST',
+      body: {
+        fname: fname.value,
+        lname: lname.value,
+        email: email.value,
+        password: password.value,
+        confirmPassword: confirmPassword.value
+      }
+    })
+    status.value = 'Account created. Please sign in.'
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      status.value = error.message
+    } else {
+      status.value = 'Unknown error'
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Sign Up</h1>
+    <form
+      method="post"
+      @submit.prevent="signUp"
+    >
+      <label for="fname">First Name</label>
+      <input
+        id="fname"
+        v-model="fname"
+        type="text"
+        name="fname"
+      >
+      <label for="lname">Last Name</label>
+      <input
+        id="lname"
+        v-model="lname"
+        type="text"
+        name="lname"
+      >
+      <label for="email">Email</label>
+      <input
+        id="email"
+        v-model="email"
+        type="email"
+        name="email"
+      >
+      <label for="password">Password</label>
+      <input
+        id="password"
+        v-model="password"
+        type="password"
+        name="password"
+      >
+      <label for="confirmPassword">Confirm Password</label>
+      <input
+        id="confirmPassword"
+        v-model="confirmPassword"
+        type="password"
+        name="confirmPassword"
+      >
+      <button type="submit">
+        Sign Up
+      </button>
+      <p v-if="status">
+        {{ status }}
+      </p>
+    </form>
+  </div>
+</template>
