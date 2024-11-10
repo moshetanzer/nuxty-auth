c0ccbef41d8ff05990679abad967e36755833a81
Moshe Tanzer <tanzermt@gmail.com>
2024-11-10 21:42:31 +0200

    chore: remove console.log

--- app/middleware/protected-from-logged-in.ts
+++ app/middleware/protected-from-logged-in.ts
@@ -1,6 +1,5 @@
 export default defineNuxtRouteMiddleware(() => {
   const { user } = useAuth()
-  console.log('protected-from-logged-in', user.value)
   if (user.value) {
     return navigateTo('/')
   }
